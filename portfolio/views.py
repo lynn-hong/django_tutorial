e1d9ee4d356ce318aef78a069b4b57746ce36944
lynn-hong <lynnn.hong@gmail.com>
2018-01-27 06:21:10 +0000

    화면에 about이 자동으로 렌더링되도록 수정(#2)

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,36 @@ from .models import About, Contact, Domain, Experience, ExperienceType, Project,
 
 class Index(TemplateView):
     template_name = 'portfolio/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+
+        # desc. text of myself
+        context['about'] = About.objects.first()
+
+        '''
+        # contact info
+        context['contacts'] = Contact.objects.all()
+
+        # domain
+        context['domains'] = get_domain()
+
+        # work experience
+        context['works'] = get_ex("work")
+
+        # educations
+        context['edus'] = get_ex("edu")
+
+        # projects
+        context['projects'] = get_project()
+
+        # papers
+        context['papers'] = get_publication("paper")
+
+        # reports
+        context['reports'] = get_publication("report")
+
+        # awards
+        context['awards'] = get_award()'''
+
+        return context
